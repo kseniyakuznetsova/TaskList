bb87be9ccac7a1c4be50026f909d184142c41cc6
Kseniya Kuznetsova <kseniya9696@gmail.com>
2018-01-23 15:35:39 +0400

    Add first database initialization

--- TaskList/TaskList/Global.asax.cs
+++ TaskList/TaskList/Global.asax.cs
@@ -14,6 +14,7 @@ namespace TaskList
     {
         protected void Application_Start()
         {
+            Database.SetInitializer(new DbInitializer());
             AreaRegistration.RegisterAllAreas();
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
---
+++ TaskList/TaskList/Models/DbInitializer.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+
+namespace TaskList.Models
+{
+    public class DbInitializer: CreateDatabaseIfNotExists<TaskContext>
+    {
+        protected override void Seed(TaskContext context)
+        {
+            context.Tasks.Add(new Task { Name = "Task1", IsCompleted = true });
+            context.Tasks.Add(new Task { Name = "Task2", IsCompleted = false });
+            context.Tasks.Add(new Task { Name = "Task3", IsCompleted = false });
+            base.Seed(context);
+        }
+    }
+}
