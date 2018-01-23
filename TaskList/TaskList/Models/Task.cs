a674c62109ae715100edc569995cb6df8ce3643d
Kseniya Kuznetsova <kseniya9696@gmail.com>
2018-01-23 14:43:26 +0400

    Add model

---
+++ TaskList/TaskList/Models/Task.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace TaskList.Models
+{
+    public class Task
+    {
+        public int Id { get; set; }
+        [Required]
+        [StringLength(50, MinimumLength = 3, ErrorMessage = "Invalid name length")]
+        public string Name { get; set; }
+        public bool IsCompleted { get; set; }
+    }
+}
